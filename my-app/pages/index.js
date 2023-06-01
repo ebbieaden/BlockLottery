9b1111686f5a3ba2a6e09588c803c61f291baeba
ebbieaden <47478940+ebbieaden@users.noreply.github.com>
2023-06-01 12:08:18 +0100

    index.js bugs fixed

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -3,65 +3,66 @@ import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
 import { abi, RANDOM_GAME_NFT_CONTRACT_ADDRESS } from "../constants";
-import { FETCH_CREATED_GAME } from "@/queries";
+import { FETCH_CREATED_GAME } from "../queries";
 import styles from "../styles/Home.module.css";
-import { subgraphQuery } from "@/utils";
+import { subgraphQuery } from "../utils";
 
 export default function Home() {
   const zero = BigNumber.from("0");
-  // walletConneced keep traxk of whether the user's wallet is connected or not
+  // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
   // loading is set to true when we are waiting for a transaction to get mined
   const [loading, setLoading] = useState(false);
   // boolean to keep track of whether the current connected account is owner or not
   const [isOwner, setIsOwner] = useState(false);
-  // entryFee is the ether requyired to enter a game
+  // entryFee is the ether required to enter a game
   const [entryFee, setEntryFee] = useState(zero);
   // maxPlayers is the max number of players that can play the game
   const [maxPlayers, setMaxPlayers] = useState(0);
-  // checks if a game is started or not
+  // Checks if a game started or not
   const [gameStarted, setGameStarted] = useState(false);
-  // players that joined the game
-  const [players, setPlayers]  = useState([]);
+  // Players that joined the game
+  const [players, setPlayers] = useState([]);
   // Winner of the game
   const [winner, setWinner] = useState();
-  // Keeep a track of all the logs for a given game
+  // Keep a track of all the logs for a given game
   const [logs, setLogs] = useState([]);
-  // create a reference to the Web 3 Modal (used for connecting to Metamask) which persists as long as the page is opened
+  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
   const web3ModalRef = useRef();
 
   // This is used to force react to re render the page when we want to
   // in our case we will use force update to show new logs
   const forceUpdate = React.useReducer(() => ({}), {})[1];
 
-  /**
-   * connectWallet: connects the wallet
-   */
+  /*
+    connectWallet: Connects the MetaMask wallet
+  */
   const connectWallet = async () => {
     try {
-      // Get the provider from web3Modal, which in our case is metamask
+      // Get the provider from web3Modal, which in our case is MetaMask
       // When used for the first time, it prompts the user to connect their wallet
       await getProviderOrSigner();
       setWalletConnected(true);
     } catch (err) {
-      console.log(err)
+      console.error(err);
     }
   };
 
-  /** Returns a Provider or Signer object representing the Ethereum RPC with or without the
+  /**
+   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
    * signing capabilities of metamask attached
-   * 
-   * A `Provider` is needed to interact with the Blockchain - reading transactions, reading balances, reading state etc
-   * 
-   * A `Signer` is a special type of Provider used in case a 'write' transaction needs to be made to the blockchain, which involves
-   * connecting the account needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website
-   * make a request signatures from the user using the Signer functions
-   * 
+   *
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
    * @param {*} needSigner - True if you need the signer, default false otherwise
    */
-  const getProviderOrSigner = async () => {
-    // connect to metamask
-    // since we are using 'web3Modal' as a reference, wee need to access the 'current' value to get access to the underlying object
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
 
@@ -69,7 +70,7 @@ export default function Home() {
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 80001) {
       window.alert("Change the network to Mumbai");
-      throw new Error("Change the network to Mumbai");
+      throw new Error("Change network to Mumbai");
     }
 
     if (needSigner) {
@@ -84,11 +85,12 @@ export default function Home() {
    */
   const startGame = async () => {
     try {
-      // Get the signer from web3Modal, which in our case is Metamask
+      // Get the signer from web3Modal, which in our case is MetaMask
       // No need for the Signer here, as we are only reading state from the blockchain
       const signer = await getProviderOrSigner(true);
-      // We connect to the Contract using a signet because we want the owner to sign the transaction
-      const randomGameNFTContract = new Contract (
+      // We connect to the Contract using a signer because we want the owner to
+      // sign the transaction
+      const randomGameNFTContract = new Contract(
         RANDOM_GAME_NFT_CONTRACT_ADDRESS,
         abi,
         signer
@@ -105,21 +107,22 @@ export default function Home() {
   };
 
   /**
-   * joinGame: is called by a player to join the game
+   * startGame: Is called by a player to join the game
    */
   const joinGame = async () => {
     try {
-      // Get the signer from web3Modal, which in our case is Metamask
+      // Get the signer from web3Modal, which in our case is MetaMask
       // No need for the Signer here, as we are only reading state from the blockchain
       const signer = await getProviderOrSigner(true);
-      // We connect to the Contract using a signer because we want the owner to sign the transaction
+      // We connect to the Contract using a signer because we want the owner to
+      // sign the transaction
       const randomGameNFTContract = new Contract(
         RANDOM_GAME_NFT_CONTRACT_ADDRESS,
         abi,
         signer
       );
       setLoading(true);
-      // call the joinGame function from the contract
+      // call the startGame function from the contract
       const tx = await randomGameNFTContract.joinGame({
         value: entryFee,
       });
@@ -132,37 +135,45 @@ export default function Home() {
   };
 
   /**
-   * checkIfGameStarted: checks is the game has started or not and initializes the logs for the game
+   * checkIfGameStarted checks if the game has started or not and intializes the logs
+   * for the game
    */
   const checkIfGameStarted = async () => {
     try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
       const provider = await getProviderOrSigner();
-      // We connect to the Contract using only a provider, so we will only have read access to the contract
+      // We connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
       const randomGameNFTContract = new Contract(
         RANDOM_GAME_NFT_CONTRACT_ADDRESS,
         abi,
         provider
       );
-      // read the gameStarted boolean for the contract
+      // read the gameStarted boolean from the contract
       const _gameStarted = await randomGameNFTContract.gameStarted();
+
       const _gameArray = await subgraphQuery(FETCH_CREATED_GAME());
       const _game = _gameArray.games[0];
       let _logs = [];
-      // Initialize the logs array and query the grapg for current gameID
+      // Initialize the logs array and query the graph for current gameID
       if (_gameStarted) {
         _logs = [`Game has started with ID: ${_game.id}`];
         if (_game.players && _game.players.length > 0) {
           _logs.push(
-            `${_game.players.length} / ${_game.maxPlayers} already joined  👀`
+            `${_game.players.length} / ${_game.maxPlayers} already joined 👀 `
           );
+          _game.players.forEach((player) => {
+            _logs.push(`${player} joined 🏃‍♂️`);
+          });
         }
         setEntryFee(BigNumber.from(_game.entryFee));
         setMaxPlayers(_game.maxPlayers);
       } else if (!gameStarted && _game.winner) {
         _logs = [
           `Last game has ended with ID: ${_game.id}`,
-          `Winner is: ${_game.winner} 🎉`,
-          `Waiting for host to start a new game...`
+          `Winner is: ${_game.winner} 🎉 `,
+          `Waiting for host to start new game....`,
         ];
 
         setWinner(_game.winner);
@@ -177,42 +188,46 @@ export default function Home() {
   };
 
   /**
-   *getOwner: calls the contract to retrieve the owner 
+   * getOwner: calls the contract to retrieve the owner
    */
   const getOwner = async () => {
     try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
       const provider = await getProviderOrSigner();
-      const randomGameNFTContract = new Contract (
+      // We connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
+      const randomGameNFTContract = new Contract(
         RANDOM_GAME_NFT_CONTRACT_ADDRESS,
         abi,
         provider
       );
-      // call the owner from the contract
+      // call the owner function from the contract
       const _owner = await randomGameNFTContract.owner();
-      // We will get the signer now to extract the address of the currently connected Metamask account
+      // We will get the signer now to extract the address of the currently connected MetaMask account
       const signer = await getProviderOrSigner(true);
-      // Get the address associated to the signer which is connected to Metamask
+      // Get the address associated to the signer which is connected to  MetaMask
       const address = await signer.getAddress();
       if (address.toLowerCase() === _owner.toLowerCase()) {
         setIsOwner(true);
       }
     } catch (err) {
-      console.error(err);
+      console.error(err.message);
     }
   };
 
   // useEffects are used to react to changes in state of the website
   // The array at the end of function call represents what state changes will trigger this effect
-  // In this case, whenever the value of 'walletConnected' changes - this effect will be called
+  // In this case, whenever the value of `walletConnected` changes - this effect will be called
   useEffect(() => {
-    // if wallet is not connected, create an instance of Web3Modal and connect the MetaMask wallet
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
     if (!walletConnected) {
-      // Assign the Web3Modal class to the reference object by setting it's 'current' value
-      // The 'current' value is persisted throughout as long as this page is open
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
         network: "mumbai",
         providerOptions: {},
-        disableInjectedProvider: false
+        disableInjectedProvider: false,
       });
       connectWallet();
       getOwner();
@@ -223,9 +238,9 @@ export default function Home() {
     }
   }, [walletConnected]);
 
-  /**
-   * renderButton: returns a button based on the state of the dapp
-   */
+  /*
+    renderButton: Returns a button based on the state of the dapp
+  */
   const renderButton = () => {
     // If wallet is not connected, return a button which allows them to connect their wallet
     if (!walletConnected) {
@@ -319,7 +334,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>Made with &#10084; by Ebbie Aden</footer>
+      <footer className={styles.footer}>Made with &#10084; Ebbie Aden</footer>
     </div>
   );
 }
